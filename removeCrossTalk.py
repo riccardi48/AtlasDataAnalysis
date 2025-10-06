e53c088f10052a572215e02cb2c0928869470f66
riccardi48 <richardballard00@gmail.com>
2025-10-06 15:48:24 +0100

    Small Rewrite of crosstalk export code

--- removeCrossTalk.py
+++ removeCrossTalk.py
@@ -1,10 +1,10 @@
-from dataAnalysis import initDataFiles
-import dataAnalysis.configLoader as configLoader
-import glob
+from dataAnalysis import initDataFiles,configLoader
 
-configLoader.saveConfig({"filterDict":{"telescope":"kit","fileName":"angle6_4Gev_kit_2"}},path="config.json")
+if __name__ == "__main__":
+    configLoader.saveConfig({"filterDict":{"telescope":"kit","fileName":"angle6_6Gev_kitHV6_kit_10"}},path="config.json")
+
 config = configLoader.loadConfig(path="config.json")
-
 dataFiles = initDataFiles(config)
+
 for dataFile in dataFiles:
     dataFile.save_nonCrossTalk_to_csv(config["pathToDataOutput"])
